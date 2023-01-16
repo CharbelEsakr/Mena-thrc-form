5786b1f9dab8c34cf594afb354d073888f716e5c
Charbel Sakr <105158568+CharbelEsakr@users.noreply.github.com>
2023-01-16 23:05:13 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('mysql-105472-0.cloudclusters.net','admin','edOWoA59','Menathrc','17206',);
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("INSERT INTO registration(firstName, lastName, gender, email, password, number) VALUES(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
